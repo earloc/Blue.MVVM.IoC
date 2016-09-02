f68623222fc76ea0390f38d20290e8aa5efcd5b8
earloc <earloc@live.de>
2016-09-02 22:13:55 +0200

    moved extensions to expected namespace

--- _Blue.MVVM.IoC/ITypeResolverExtensions.cs
+++ _Blue.MVVM.IoC/ITypeResolverExtensions.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blue.MVVM.Extensions {
+namespace Blue.MVVM.IoC {
 
     /// <summary>
     /// Extensions for ITypeResolver
